b30dbdcb75e58bd2d5ec8ce6f810e04332118315
lw0312 <128790977+lw0312@users.noreply.github.com>
2023-05-26 23:36:42 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import { Loading } from 'vant';
-import 'vant/lib/index.css';
-
-import App from './App.vue'
-const app = createApp(App)
-
-app.use(Loading)
-app.mount('#app')
