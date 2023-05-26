5d4f44c4bfb687276cd8afc0d14ffab36b83f394
lw0312 <128790977+lw0312@users.noreply.github.com>
2023-05-26 23:54:06 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import { Loading } from 'vant';
 import 'vant/lib/index.css';
 import { inject } from '@vercel/analytics';
-inject();
 import App from './App.vue'
 const app = createApp(App)
 app.use(Loading)
+app.use(inject)
 app.mount('#app')
